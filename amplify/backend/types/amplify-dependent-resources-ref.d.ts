bb722a18a93f1a4f62f30e0a821fab6bdc8ea8a8
Minh Duc Nguyen <duc.nguyenminh@pionero.io>
2024-05-04 16:41:44 +0700

    aws amplify, dynamodb, appsync tutorial

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplifytutorial": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { generateClient } from "aws-amplify/api";
+
 import logo from "./logo.svg";
 import "./App.css";
+import * as queries from "./graphql/queries";
+import * as mutations from "./graphql/mutations";
+
+const API = generateClient();
 
 function App() {
+  async function fetchTodos() {
+    let response = await API.graphql({
+      query: queries.listTodos,
+    });
+
+    console.log(response);
+  }
+
+  async function createTodos() {
+    let myTodo = {
+      name: "Test 4",
+      description: "Description 4",
+    };
+
+    const response = await API.graphql({
+      query: mutations.createTodo,
+      variables: {
+        input: myTodo,
+      },
+    });
+
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +45,8 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={fetchTodos}>Fetch to do</button>
+        <button onClick={createTodos}>Create to do</button>
       </header>
     </div>
   );
